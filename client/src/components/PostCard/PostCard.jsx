76568ec258f976e5ab2fda263cfe48144f396736
jbenet <nycflasher@yahoo.com>
2021-05-24 16:01:40 -0400

    postcard

---
+++ client/src/components/PostCard/PostCard.jsx
@@ -0,0 +1,15 @@
+import './PostCard.css';
+import { Link } from 'react-router-dom'
+
+const PostCard = (props) => {
+    return (
+        <div className="post-card">
+            <Link className="card" to={`/posts/${props._id}`}>
+                <img className="product-card-image" src={props.imgURL} alt={props.name} />
+                <div>View</div>
+            </Link>
+        </div>
+    )
+}
+
+export default PostCard
