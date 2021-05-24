2505fe97185510aa764221781ac2a03321d94692
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-24 16:33:37 -0400

    screens part1

---
+++ client/src/screens/Posts/Posts.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import './Posts.css'
+
+import Post from '../../components/Post/Post'
+import Search from '../../components/Search/Search'
+import Layout from '../../components/Layout/Layout'
+import { getPosts } from '../../services/posts'
+
+const Posts = () => {
+  const [posts, setPosts] = useState([])
+  const [searchResult, setSearchResult] = useState([])
+  
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const allPosts = await getPosts()
+      setPosts(allPosts)
+      setSearchResult(allPosts)
+    }
+    fetchPosts()
+  }, [])
+
+ 
+  const handleSearch = (event) => {
+    const results = posts.filter((post) =>
+      post.name.toLowerCase().includes(event.target.value.toLowerCase())
+    )
+    setSearchResult(results)
+  }
+
+  const handleSubmit = (event) => event.preventDefault()
+
+  return (
+    <Layout>
+      <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
+      <Sort onSubmit={handleSubmit} handleSort={handleSort} />
+      <div className='posts'>
+        {searchResult.map((post, index) => {
+          return (
+            <Post
+              _id={post._id}
+              title={post.title}
+              imgURL={post.imgURL}
+              author={post.author}
+              key={index}
+            />
+          )
+        })}
+      </div>
+    </Layout>
+  )
+}
+
+export default Posts
