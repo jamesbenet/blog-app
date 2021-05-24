ab456bd78a4293fb0927254d779f6dc1d451729d
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-24 15:53:51 -0400

    components work #2

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -6,8 +6,8 @@ const Post = (props) => {
         <>
             <Link className="post" to={`/posts/${props._id}`}>
                 <img className="post-image" src={props.imgURL} alt={props.name} />
-                <div className="post-name">{props.name}</div>
-                <div className="price">{`$${props.price}`}</div>
+                <div className="post-title">{props.title}</div>
+                <div className="post-author">{`$${props.author}`}</div>
             </Link>
         </>
     )
